fe5521910c533febaa5375a4686b2bf522077148
Vojtech Simetka <vojtech@simetka.cz>
2022-11-10 12:29:09 +0100

    feat: add typechain

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import { Provider } from '@ethersproject/abstract-provider'
-import {Contract, Signer} from 'ethers'
+import { Signer} from 'ethers'
 
-import ERC20 from './abis/MintableERC20.json'
-import MarketplaceFactory from './abis/MarketplaceFactory.json'
-import MarketplaceList from './abis/MarketplaceList.json'
+import { factories } from './abi'
 
-export const getERC20 = (address: string, signer?: Signer | Provider) => new Contract(address, ERC20.abi, signer)
-export const getMarketplaceFactory = (address: string, signer?: Signer | Provider) => new Contract(address, MarketplaceFactory.abi, signer)
-export const getMarketplaceList = (address: string, signer?: Signer | Provider) => new Contract(address, MarketplaceList.abi, signer)
+export const getERC20 = (address: string, signer: Signer | Provider) => factories.MintableERC20__factory.connect(address, signer)
+export const getMarketplaceFactory = (address: string, signer: Signer | Provider) => factories.MarketplaceFactory__factory.connect(address, signer)
+export const getMarketplaceList = (address: string, signer: Signer | Provider) => factories.MarketplaceList__factory.connect(address, signer)
