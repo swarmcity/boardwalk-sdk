efda019632d7518e80a8e9943f18de046d1085da
Vojtech Simetka <vojtech@simetka.cz>
2022-11-10 15:33:09 +0100

    feat: add create marketplace function

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,41 @@
 import { Provider } from '@ethersproject/abstract-provider'
-import { Signer} from 'ethers'
+import { BigNumberish, Signer} from 'ethers'
 
-import { factories } from './abi'
+import { factories, MarketplaceFactory, MintableERC20 } from './abi'
+import { Marketplace } from './abi/Marketplace'
 
 export const getERC20 = (address: string, signer: Signer | Provider) => factories.MintableERC20__factory.connect(address, signer)
 export const getMarketplaceFactory = (address: string, signer: Signer | Provider) => factories.MarketplaceFactory__factory.connect(address, signer)
 export const getMarketplaceList = (address: string, signer: Signer | Provider) => factories.MarketplaceList__factory.connect(address, signer)
+export const getMarketplace = (address: string, signer: Signer | Provider) => factories.Marketplace__factory.connect(address, signer)
+
+interface EventMarketplaceCreatedData {
+    marketplaceAddress: string
+    marketplaceName: string
+    providerRepAddress: string
+    seekerRepAddress: string
+}
+
+interface CreateMarketplace {
+    marketplace: Marketplace
+    providerRepToken: MintableERC20
+    seekerRepToken: MintableERC20
+}
+
+export async function createMarketplace(marketplaceFactory: MarketplaceFactory, marketplaceTokenAddress: string, marketplaceName: string, marketplaceFee: BigNumberish, metadata: string): Promise<CreateMarketplace> {
+        let promise = new Promise<EventMarketplaceCreatedData>((resolve) => {
+            marketplaceFactory.once(
+                marketplaceFactory.filters['MarketplaceCreated'](),
+                (marketplaceAddress, marketplaceName, providerRepAddress, seekerRepAddress) => {
+                    resolve({marketplaceAddress, marketplaceName, providerRepAddress, seekerRepAddress})
+                }
+            )})
+        const marketplace = await marketplaceFactory.create(marketplaceTokenAddress, marketplaceName, marketplaceFee, metadata)
+        const res = await promise
+
+        return {
+            marketplace: getMarketplace(res.marketplaceAddress, marketplaceFactory.signer),
+            providerRepToken: getERC20(res.providerRepAddress, marketplaceFactory.signer),
+            seekerRepToken: getERC20(res.seekerRepAddress, marketplaceFactory.signer)
+        }
+}
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,9 +1,10 @@
-import { JsonRpcProvider } from '@ethersproject/providers'
-import { Wallet } from 'ethers'
-import {getMarketplaceFactory, getERC20 } from '../src/index'
+import { WebSocketProvider } from '@ethersproject/providers'
+import { BigNumberish, Wallet } from 'ethers'
+import {getMarketplaceFactory, getERC20, createMarketplace } from '../src/index'
 import { MARKETPLACE_FACTORY_ADDRESS, MARKETPLACE_TOKEN_ADDRESS, PRIVATE_KEYS } from './test-addresses.json'
+import { sleep } from './utils'
 
-const provider = new JsonRpcProvider('http://127.0.0.1:8545')
+const provider = new WebSocketProvider('ws://127.0.0.1:8545')
 const marketplaceFactoryDeployer = new Wallet(PRIVATE_KEYS[0], provider)
 const marketplaceFactory = getMarketplaceFactory(MARKETPLACE_FACTORY_ADDRESS, marketplaceFactoryDeployer)
 const marketplaceToken = getERC20(MARKETPLACE_TOKEN_ADDRESS, marketplaceFactoryDeployer)
@@ -16,8 +17,20 @@ describe('MarketplaceToken', () => {
     })
 })
 
+interface EventData {
+    marketplaceAddress: string
+    marketplaceName: string
+    providerRepAddress: string
+    seekerRepAddress: string
+}
+
 describe('MarketplaceFactory', () => {
     test('should create new marketplace ', async () => {
-        expect(marketplaceFactory.address).toEqual(MARKETPLACE_FACTORY_ADDRESS)
+        const marketplaceName = 'testmarketplace'
+        const marketplaceFee = 0
+        const marketplace = await createMarketplace(marketplaceFactory, marketplaceToken.address, marketplaceName, marketplaceFee, '')
+        expect(marketplace.marketplace.address).toMatch(/0x[a-z0-9]{40}/i)
+        expect(marketplace.providerRepToken.address).toMatch(/0x[a-z0-9]{40}/i)
+        expect(marketplace.seekerRepToken.address).toMatch(/0x[a-z0-9]{40}/i)
     })
 })
