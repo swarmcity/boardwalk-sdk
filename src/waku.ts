9f71ba70be887b6dfb26187f4fe27493ff674e6b
Philippe Schommers <philippe@schommers.be>
2022-11-15 22:14:33 +0100

    test: add basic lightPush + filter test

--- src/waku.ts
+++ src/waku.ts
@@ -3,13 +3,15 @@ import { createLightNode, CreateOptions } from "js-waku/lib/create_waku";
 
 import type { WakuLight } from "js-waku/lib/interfaces";
 
-const defaultOptions: CreateOptions = {}
+const defaultOptions: CreateOptions = {};
 
 export async function getWaku(options?: CreateOptions): Promise<WakuLight> {
   const waku = await createLightNode(options ?? defaultOptions);
 
   await waku.start();
-  await waku.dial(multiaddr("/ip4/0.0.0.0/tcp/60000"))
-  
+
+  // @ts-expect-error Some weird bug with [inspect]
+  await waku.dial(multiaddr("/ip4/0.0.0.0/tcp/60000"));
+
   return waku;
 }
--- test/waku.spec.ts
+++ test/waku.spec.ts
@@ -1,9 +1,35 @@
-import { getWaku } from '../src/waku'
+import { getWaku } from "../src/waku";
+import {
+  DecoderV0,
+  EncoderV0,
+  MessageV0,
+} from "js-waku/lib/waku_message/version_0";
+
+type CleanUpFunction = () => Promise<void>;
 
 describe("waku", () => {
-  test("send and receive message", async () => {
-    const waku = await getWaku()
+  let cleanupFns: CleanUpFunction[] = [];
 
-    console.log(waku.isStarted());
+  afterEach(async () => {
+    await Promise.all(cleanupFns.map((fn) => fn()));
+    cleanupFns = [];
+  });
+
+  test("send and receive message over PubSub", async () => {
+    const waku = await getWaku();
+    const encoder = new TextEncoder();
+    const payload = encoder.encode("Test message");
+
+    const promise = new Promise<MessageV0>((resolve) => {
+      const unsubscribe = waku.filter.subscribe(
+        [new DecoderV0("topic")],
+        resolve
+      );
+      unsubscribe.then(cleanupFns.push);
+    });
+
+    waku.lightPush.push(new EncoderV0("topic"), { payload });
+    const message = await promise;
+    expect(message.payload).toEqual(payload);
   });
 });
