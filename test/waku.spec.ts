07c0b5a4a653fb2190990223513f1e57c5edbe14
Philippe Schommers <philippe@schommers.be>
2022-11-16 15:26:40 +0100

    test: add Waku store test

--- test/waku.spec.ts
+++ test/waku.spec.ts
@@ -37,4 +37,23 @@ describe("waku", () => {
     const message = await deferred.promise;
     expect(message.payload).toEqual(payload);
   });
+
+  test("push a message an fetch it from the store", async () => {
+    const waku = await getWaku([Protocols.LightPush, Protocols.Store]);
+    const encoder = new TextEncoder();
+    const payload = encoder.encode("Test message");
+    const topic = Math.random().toString();
+
+    await waku.lightPush.push(new EncoderV0(topic), {
+      payload,
+    });
+
+    const generator = await waku.store.queryGenerator([new DecoderV0(topic)]);
+    const { value, done } = await generator.next();
+    expect(done).toBeTruthy;
+    expect(value).toHaveLength(1);
+
+    const message = await value[0];
+    expect(message.payload).toEqual(payload);
+  });
 });
