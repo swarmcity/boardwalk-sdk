68bbf2a1c8a48b6f8367123c98fe1ff5f0f27adb
Vojtech Simetka <vojtech@simetka.cz>
2022-11-16 17:10:06 +0100

    chore: add prettier config (#5)

--- src/waku.ts
+++ src/waku.ts
@@ -1,26 +1,26 @@
-import { multiaddr } from "@multiformats/multiaddr";
-import { Protocols } from "js-waku";
-import { createLightNode, CreateOptions } from "js-waku/lib/create_waku";
-import { waitForRemotePeer } from "js-waku/lib/wait_for_remote_peer";
+import { multiaddr } from '@multiformats/multiaddr'
+import { Protocols } from 'js-waku'
+import { createLightNode, CreateOptions } from 'js-waku/lib/create_waku'
+import { waitForRemotePeer } from 'js-waku/lib/wait_for_remote_peer'
 
-import type { WakuLight } from "js-waku/lib/interfaces";
+import type { WakuLight } from 'js-waku/lib/interfaces'
 
-const defaultOptions: CreateOptions = {};
+const defaultOptions: CreateOptions = {}
 
 export async function getWaku(
-  protocols?: Protocols[],
-  options?: CreateOptions
+	protocols?: Protocols[],
+	options?: CreateOptions,
 ): Promise<WakuLight> {
-  const waku = await createLightNode(options ?? defaultOptions);
+	const waku = await createLightNode(options ?? defaultOptions)
 
-  await waku.start();
-  await waku.dial(
-    // @ts-expect-error Some weird bug with [inspect]
-    multiaddr(
-      "/ip4/127.0.0.1/tcp/8000/ws/p2p/16Uiu2HAm53sojJN72rFbYg6GV2LpRRER9XeWkiEAhjKy3aL9cN5Z"
-    )
-  );
-  await waitForRemotePeer(waku, protocols);
+	await waku.start()
+	await waku.dial(
+		// @ts-expect-error Some weird bug with [inspect]
+		multiaddr(
+			'/ip4/127.0.0.1/tcp/8000/ws/p2p/16Uiu2HAm53sojJN72rFbYg6GV2LpRRER9XeWkiEAhjKy3aL9cN5Z',
+		),
+	)
+	await waitForRemotePeer(waku, protocols)
 
-  return waku;
+	return waku
 }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,53 +1,46 @@
-import { WebSocketProvider } from "@ethersproject/providers";
-import { Wallet } from "ethers";
-import { describe, test, afterAll, expect } from "vitest";
+import { WebSocketProvider } from '@ethersproject/providers'
+import { Wallet } from 'ethers'
+import { describe, test, afterAll, expect } from 'vitest'
+import { getMarketplaceFactory, getERC20, createMarketplace } from '../src/index'
 import {
-  getMarketplaceFactory,
-  getERC20,
-  createMarketplace,
-} from "../src/index";
-import {
-  MARKETPLACE_FACTORY_ADDRESS,
-  MARKETPLACE_TOKEN_ADDRESS,
-  PRIVATE_KEYS,
-} from "./test-addresses.json";
+	MARKETPLACE_FACTORY_ADDRESS,
+	MARKETPLACE_TOKEN_ADDRESS,
+	PRIVATE_KEYS,
+} from './test-addresses.json'
 
-const provider = new WebSocketProvider("ws://127.0.0.1:8545");
-const marketplaceFactoryDeployer = new Wallet(PRIVATE_KEYS[0], provider);
+const provider = new WebSocketProvider('ws://127.0.0.1:8545')
+const marketplaceFactoryDeployer = new Wallet(PRIVATE_KEYS[0], provider)
 const marketplaceFactory = getMarketplaceFactory(
-  MARKETPLACE_FACTORY_ADDRESS,
-  marketplaceFactoryDeployer
-);
-const marketplaceToken = getERC20(
-  MARKETPLACE_TOKEN_ADDRESS,
-  marketplaceFactoryDeployer
-);
+	MARKETPLACE_FACTORY_ADDRESS,
+	marketplaceFactoryDeployer,
+)
+const marketplaceToken = getERC20(MARKETPLACE_TOKEN_ADDRESS, marketplaceFactoryDeployer)
 
 afterAll(async () => {
-  await provider.destroy();
-});
+	await provider.destroy()
+})
 
-describe("MarketplaceToken", () => {
-  test("is successfully deployed ", async () => {
-    expect(await marketplaceToken.decimals()).toEqual(18);
-    expect(await marketplaceToken.name()).toEqual("testDAI");
-    expect(await marketplaceToken.symbol()).toEqual("tDAI");
-  });
-});
+describe('MarketplaceToken', () => {
+	test('is successfully deployed ', async () => {
+		expect(await marketplaceToken.decimals()).toEqual(18)
+		expect(await marketplaceToken.name()).toEqual('testDAI')
+		expect(await marketplaceToken.symbol()).toEqual('tDAI')
+	})
+})
 
-describe("MarketplaceFactory", () => {
-  test("should create new marketplace ", async () => {
-    const marketplaceName = "testmarketplace";
-    const marketplaceFee = 0;
-    const marketplace = await createMarketplace(
-      marketplaceFactory,
-      marketplaceToken.address,
-      marketplaceName,
-      marketplaceFee,
-      ""
-    );
-    expect(marketplace.marketplace.address).toMatch(/0x[a-z0-9]{40}/i);
-    expect(marketplace.providerRepToken.address).toMatch(/0x[a-z0-9]{40}/i);
-    expect(marketplace.seekerRepToken.address).toMatch(/0x[a-z0-9]{40}/i);
-  });
-});
+describe('MarketplaceFactory', () => {
+	test('should create new marketplace ', async () => {
+		const marketplaceName = 'testmarketplace'
+		const marketplaceFee = 0
+		const marketplace = await createMarketplace(
+			marketplaceFactory,
+			marketplaceToken.address,
+			marketplaceName,
+			marketplaceFee,
+			'',
+		)
+		expect(marketplace.marketplace.address).toMatch(/0x[a-z0-9]{40}/i)
+		expect(marketplace.providerRepToken.address).toMatch(/0x[a-z0-9]{40}/i)
+		expect(marketplace.seekerRepToken.address).toMatch(/0x[a-z0-9]{40}/i)
+	})
+})
