9eb80f027e7b9659d0602e1c76ea2de55a205015
Philippe Schommers <philippe@schommers.be>
2022-11-16 15:22:00 +0100

    fix: waku pubsub test

--- src/waku.ts
+++ src/waku.ts
@@ -1,21 +1,26 @@
 import { multiaddr } from "@multiformats/multiaddr";
+import { Protocols } from "js-waku";
 import { createLightNode, CreateOptions } from "js-waku/lib/create_waku";
+import { waitForRemotePeer } from "js-waku/lib/wait_for_remote_peer";
 
 import type { WakuLight } from "js-waku/lib/interfaces";
 
 const defaultOptions: CreateOptions = {};
 
-export async function getWaku(options?: CreateOptions): Promise<WakuLight> {
+export async function getWaku(
+  protocols?: Protocols[],
+  options?: CreateOptions
+): Promise<WakuLight> {
   const waku = await createLightNode(options ?? defaultOptions);
 
   await waku.start();
-
   await waku.dial(
     // @ts-expect-error Some weird bug with [inspect]
     multiaddr(
-      "/ip4/127.0.0.1/tcp/60000/ws/p2p/16Uiu2HAmV2LtgL5Rx4WhnMShcZbXadkT8Lfa2GC38aJ7hDzKhPuo"
+      "/ip4/127.0.0.1/tcp/8000/ws/p2p/16Uiu2HAm53sojJN72rFbYg6GV2LpRRER9XeWkiEAhjKy3aL9cN5Z"
     )
   );
+  await waitForRemotePeer(waku, protocols);
 
   return waku;
 }
--- test/waku.spec.ts
+++ test/waku.spec.ts
@@ -5,6 +5,8 @@ import {
   EncoderV0,
   MessageV0,
 } from "js-waku/lib/waku_message/version_0";
+import { Protocols } from "js-waku";
+import pDefer from "p-defer";
 
 type CleanUpFunction = () => Promise<void>;
 
@@ -17,20 +19,22 @@ describe("waku", () => {
   });
 
   test("send and receive message over PubSub", async () => {
-    const waku = await getWaku();
+    const waku = await getWaku([Protocols.LightPush, Protocols.Filter]);
     const encoder = new TextEncoder();
     const payload = encoder.encode("Test message");
+    const deferred = pDefer<MessageV0>();
 
-    const promise = new Promise<MessageV0>((resolve) => {
-      const unsubscribe = waku.filter.subscribe(
-        [new DecoderV0("topic")],
-        resolve
-      );
-      unsubscribe.then(cleanupFns.push);
+    const unsubscribe = await waku.filter.subscribe(
+      [new DecoderV0("topic")],
+      deferred.resolve
+    );
+    cleanupFns.push(unsubscribe);
+
+    await waku.lightPush.push(new EncoderV0("topic"), {
+      payload,
     });
 
-    waku.lightPush.push(new EncoderV0("topic"), { payload });
-    const message = await promise;
+    const message = await deferred.promise;
     expect(message.payload).toEqual(payload);
   });
 });
