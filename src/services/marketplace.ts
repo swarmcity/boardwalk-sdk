216c56a867928e525c9ac9d60446101a3ef1d5ae
Philippe Schommers <philippe@schommers.be>
2022-11-28 14:46:48 +0100

    feat: convert marketplace service

--- src/services/marketplace.ts
+++ src/services/marketplace.ts
@@ -1,28 +1,19 @@
-import { BigNumber, BigNumberish, Contract, Signer } from 'ethers'
-import { useEffect, useMemo, useState } from 'react'
-import { useProvider, useWebSocketProvider } from 'wagmi'
-import {
-	JsonRpcBatchProvider,
-	JsonRpcProvider,
-	Provider,
-} from '@ethersproject/providers'
+import { BigNumber, BigNumberish, Signer } from 'ethers'
+
+// Types
+import type { Provider } from '@ethersproject/providers'
 
 // ABIs
-import marketplaceAbi from '../../../abis/marketplace.json'
-import erc20Abi from '../../../abis/erc20.json'
+import { Marketplace__factory } from '../abi'
+import type { FundItemEvent, ItemStatusChangeEvent } from '../abi/Marketplace'
 
 // Lib
-import { cleanOutput } from '../../../lib/ethers'
+import { cleanOutput } from '../lib/ethers'
 
 // Services
-import { Status } from './marketplace-items'
-import { useReputation } from '../../../services/reputation'
-import { getCache, useCache } from '../../../lib/cache'
-
-// Cache
-const TOKEN_NAME_CACHE: Map<string, Promise<string> | undefined> = new Map()
-const CONTRACT_NAME_CACHE: Map<string, Promise<string> | undefined> = new Map()
-const CONTRACT_TOKEN_CACHE: Map<string, Promise<string> | undefined> = new Map()
+import type { Status } from './items'
+import { getReputation } from './reputation'
+import { getERC20Contract } from './erc20'
 
 // Types
 export type MarketplaceItem = {
@@ -36,19 +27,8 @@ export type MarketplaceItem = {
 	status: Status
 }
 
-export const getMarketplaceContract = (
-	address: string,
-	signerOrProvider?: Signer | Provider
-) => {
-	return new Contract(address, marketplaceAbi, signerOrProvider)
-}
-
-export const useMarketplaceContract = (address: string) => {
-	const provider = useProvider()
-	return useMemo(
-		() => getMarketplaceContract(address, provider),
-		[address, provider]
-	)
+export const getMarketplaceContract = (address: string, signerOrProvider: Signer | Provider) => {
+	return Marketplace__factory.connect(address, signerOrProvider)
 }
 
 type MarketplaceConfig = {
@@ -62,249 +42,102 @@ type MarketplaceConfig = {
 	itemId: BigNumber
 }
 
-export const useMarketplaceConfig = <Keys extends keyof MarketplaceConfig>(
+export const getMarketplaceConfig = async <Keys extends keyof MarketplaceConfig>(
 	address: string,
-	keys: Keys[]
+	keys: Keys[],
+	provider: Provider,
 ) => {
-	// Create a batch provider for efficient lookup
-	const provider = useProvider()
-	const batchProvider = useMemo(() => {
-		return provider instanceof JsonRpcProvider
-			? new JsonRpcBatchProvider(provider.connection, provider.network)
-			: provider
-	}, [provider])
+	// TODO: Batch provider somehow?
+	/*
+	return provider instanceof JsonRpcProvider
+		? new JsonRpcBatchProvider(provider.connection, provider.network)
+		: provider
+	*/
 
 	// Get the contract using said provider
-	const contract = useMemo(
-		() => new Contract(address, marketplaceAbi, batchProvider),
-		[address, batchProvider]
+	const contract = getMarketplaceContract(address, provider)
+
+	// Fetch all required keys
+	const elements = await Promise.all(
+		keys.map(async (key) => ({ key, value: await contract[key]() })),
 	)
 
-	// Final state
-	const [config, setConfig] = useState<Pick<MarketplaceConfig, Keys>>()
-
-	useEffect(() => {
-		if (!contract.address) {
-			return
-		}
-
-		// eslint-disable-next-line @typescript-eslint/no-extra-semi
-		;(async () => {
-			// Fetch all required keys
-			const elements = await Promise.all(
-				keys.map(async (key) => ({ key, value: await contract[key]() }))
-			)
-
-			// Convert the array to a MarketplaceConfig object
-			const config = elements.reduce((result, { key, value }) => {
-				result[key] = value
-				return result
-			}, {} as Pick<MarketplaceConfig, Keys>)
-
-			setConfig(config)
-		})()
-	}, [contract, ...keys])
-
-	return config
+	// Convert the array to a MarketplaceConfig object
+	return elements.reduce((result, { key, value }) => {
+		result[key] = value as MarketplaceConfig[typeof key]
+		return result
+	}, {} as Pick<MarketplaceConfig, Keys>)
 }
 
-export const useMarketplaceName = (address: string) => {
-	const contract = useMarketplaceContract(address)
-	return useCache(CONTRACT_NAME_CACHE, address, () => contract?.name(), [
-		contract,
-	])
+export const getMarketplaceTokenContract = async (marketplace: string, provider: Provider) => {
+	const { token } = await getMarketplaceConfig(marketplace, ['token'], provider)
+	return getERC20Contract(token, provider)
 }
 
-export const getMarketplaceTokenContract = async (
+export const getMarketplaceTokenDecimals = async (address: string, provider: Provider) => {
+	const token = await getMarketplaceTokenContract(address, provider)
+	return await token.decimals()
+}
+
+export const getMarketplaceItem = async (
 	marketplace: string,
-	signerOrProvider?: Signer | Provider
+	itemId: bigint,
+	wsProvider: Provider,
+	callback?: (item: MarketplaceItem) => void,
 ) => {
-	const contract = getMarketplaceContract(marketplace, signerOrProvider)
-	const token = await getCache(
-		CONTRACT_TOKEN_CACHE,
-		marketplace,
-		contract.token
-	)
+	const contract = getMarketplaceContract(marketplace, wsProvider)
+	const item: MarketplaceItem = cleanOutput(await contract.items(itemId))
 
-	if (!token) {
-		throw new Error('cache empty, should never happen')
+	const statusFilter = contract.filters.ItemStatusChange(itemId)
+	const statusListener = (_: BigNumberish, status: Status) => {
+		item.status = status
+		callback && callback(item)
 	}
 
-	return new Contract(token, erc20Abi, signerOrProvider)
+	const fundFilter = contract.filters.FundItem(null, itemId)
+	const fundListener = (providerAddress: string) => {
+		// TODO: Fetch `providerRep`
+		item.providerAddress = providerAddress
+		item.providerRep = 0n
+		callback && callback(item)
+	}
+
+	contract.on<ItemStatusChangeEvent>(statusFilter, statusListener)
+
+	if (callback) {
+		contract.on<ItemStatusChangeEvent>(statusFilter, statusListener)
+		contract.on<FundItemEvent>(fundFilter, fundListener)
+	}
+
+	const unsubscribe = callback
+		? () => {
+				contract.off<ItemStatusChangeEvent>(statusFilter, statusListener)
+				contract.off<FundItemEvent>(fundFilter, fundListener)
+		  }
+		: () => undefined
+
+	return { item, unsubscribe }
 }
 
-export const useMarketplaceTokenContract = (marketplace?: string) => {
-	const [token, setToken] = useState<Contract | undefined>()
-	const provider = useProvider()
-
-	useEffect(() => {
-		if (marketplace) {
-			getMarketplaceTokenContract(marketplace, provider).then(setToken)
-		}
-	}, [marketplace, provider])
-
-	return token
-}
-
-export const useMarketplaceTokenDecimals = (address?: string) => {
-	const [loading, setLoading] = useState(true)
-	const [decimals, setDecimals] = useState<number | undefined>(undefined)
-
-	// Get the marketplace contract
-	const token = useMarketplaceTokenContract(address)
-
-	useEffect(() => {
-		if (!token) {
-			return
-		}
-
-		// eslint-disable-next-line @typescript-eslint/no-extra-semi
-		;(async () => {
-			setDecimals(await token.decimals())
-			setLoading(false)
-		})()
-	}, [token])
-
-	return { decimals, loading }
-}
-
-export const useMarketplaceItem = (marketplace: string, itemId: bigint) => {
-	const wsProvider = useWebSocketProvider()
-	const contract = useMarketplaceContract(marketplace)
-	const [item, setItem] = useState<MarketplaceItem>()
-	const [lastUpdate, setLastUpdate] = useState(Date.now())
-	const [loading, setLoading] = useState(true)
-
-	useEffect(() => {
-		if (!wsProvider) {
-			return
-		}
-
-		const wsContract = contract.connect(wsProvider)
-
-		const statusFilter = contract.filters.ItemStatusChange(itemId)
-		const statusListener = (_: BigNumberish, status: Status) => {
-			setItem((item) => item && { ...item, status })
-		}
-
-		const fundFilter = contract.filters.FundItem(null, itemId)
-		const fundListener = (providerAddress: string) => {
-			// TODO: Fetch `providerRep`
-			setItem((item) => item && { ...item, providerAddress, providerRep: 0n })
-		}
-
-		contract.items(itemId).then((item: Array<BigNumberish | string>) => {
-			setItem(cleanOutput(item))
-			setLastUpdate(Date.now())
-			setLoading(false)
-
-			wsContract.on(statusFilter, statusListener)
-			wsContract.on(fundFilter, fundListener)
-		})
-
-		return () => {
-			wsContract.off(statusFilter, statusListener)
-			wsContract.off(fundFilter, fundListener)
-		}
-	}, [marketplace, itemId, wsProvider])
-
-	return { item, lastUpdate, loading }
-}
-
-export const useMarketplaceSeekerReputation = (
+export const getMarketplaceSeekerReputation = async (
 	marketplace: string,
-	user?: string
+	user: string,
+	provider: Provider,
 ) => {
-	const config = useMarketplaceConfig(marketplace, ['seekerRep'])
-	return useReputation(config?.seekerRep, user)
+	const { seekerRep } = await getMarketplaceConfig(marketplace, ['seekerRep'], provider)
+	return getReputation(seekerRep, user, provider)
 }
 
-export const useMarketplaceProviderReputation = (
+export const getMarketplaceProviderReputation = async (
 	marketplace: string,
-	user?: string
+	user: string,
+	provider: Provider,
 ) => {
-	const config = useMarketplaceConfig(marketplace, ['providerRep'])
-	return useReputation(config?.providerRep, user)
+	const { providerRep } = await getMarketplaceConfig(marketplace, ['providerRep'], provider)
+	return getReputation(providerRep, user, provider)
 }
 
-export const useMarketplaceDealCount = (marketplace: string) => {
-	const config = useMarketplaceConfig(marketplace, ['itemId'])
-	return useMemo(
-		() => config?.itemId.sub(1) ?? BigNumber.from(0),
-		[config?.itemId]
-	)
-}
-
-export const useMarketplaceTokenName = (
-	address?: string
-): string | undefined => {
-	const token = useMarketplaceTokenContract(address)
-	return useCache(TOKEN_NAME_CACHE, address, () => token?.name(), [token])
-}
-
-export const useMarketplaceTokenBalanceOf = (
-	address?: string,
-	userAddress?: string
-): BigNumber | undefined => {
-	const [balance, setBalance] = useState<BigNumber | undefined>()
-	const token = useMarketplaceTokenContract(address)
-
-	useEffect(() => {
-		token?.balanceOf(userAddress).then(setBalance)
-	}, [token])
-
-	return balance
-}
-
-export const useToken = (tokenAddress?: string): Contract | undefined => {
-	const provider = useProvider()
-	const token = useMemo(
-		() =>
-			tokenAddress ? new Contract(tokenAddress, erc20Abi, provider) : undefined,
-		[tokenAddress]
-	)
-
-	return token
-}
-
-export const useTokenBalanceOf = (
-	tokenAddress?: string,
-	userAddress?: string
-): BigNumber | undefined => {
-	const [balance, setBalance] = useState<BigNumber | undefined>()
-	const token = useToken(tokenAddress)
-
-	useEffect(() => {
-		token?.balanceOf(userAddress).then(setBalance)
-	}, [token])
-
-	return balance
-}
-
-export const useTokenName = (tokenAddress?: string): string | undefined => {
-	const token = useToken(tokenAddress)
-
-	return useCache(TOKEN_NAME_CACHE, tokenAddress, () => token?.name(), [token])
-}
-
-export const useTokenDecimals = (tokenAddress?: string) => {
-	const [loading, setLoading] = useState(true)
-	const [decimals, setDecimals] = useState<number | undefined>(undefined)
-
-	// Get the marketplace contract
-	const token = useToken(tokenAddress)
-
-	useEffect(() => {
-		if (!token) {
-			return
-		}
-
-		// eslint-disable-next-line @typescript-eslint/no-extra-semi
-		;(async () => {
-			setDecimals(await token.decimals())
-			setLoading(false)
-		})()
-	}, [token])
-
-	return { decimals, loading }
+export const getMarketplaceDealCount = async (marketplace: string, provider: Provider) => {
+	const { itemId } = await getMarketplaceConfig(marketplace, ['itemId'], provider)
+	return itemId.sub(1)
 }
