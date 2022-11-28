cbfacb3ded83c9f47e2dcb8e263c73d7d98f0882
Philippe Schommers <philippe@schommers.be>
2022-11-28 15:54:46 +0100

    feat: convert items service

--- src/services/items.ts
+++ src/services/items.ts
@@ -1,5 +1,5 @@
 import { DecoderV0, EncoderV0, MessageV0 } from 'js-waku/lib/waku_message/version_0'
-import { BigNumber, Contract, Event } from 'ethers'
+import { BigNumber, Event } from 'ethers'
 import { Interface } from 'ethers/lib/utils'
 import { Log } from '@ethersproject/providers'
 
@@ -8,20 +8,18 @@ import type { Signer } from 'ethers'
 import type { WakuLight } from 'js-waku/lib/interfaces'
 import type { UpdateTime } from '../lib/blockchain'
 import type { WithPayload } from '../lib/types'
+import type { Provider } from '@ethersproject/providers'
 
 // Protos
 import { ItemMetadata } from '../protos/item-metadata'
-
-// ABIs
-import marketplaceAbi from '../../../abis/marketplace.json'
-import erc20Abi from '../../../abis/erc20.json'
 
 // Lib
 import { bufferToHex, numberToBigInt } from '../lib/tools'
 import { shouldUpdate } from '../lib/blockchain'
 
 // Services
-import { subscribeToWakuStoreQuery } from '../lib/waku'
+import { getMarketplaceContract } from './marketplace'
+import { getERC20Contract } from './erc20'
 
 // Status
 export enum Status {
@@ -79,16 +77,20 @@ export const createItem = async (
 	{ price, description }: CreateItem,
 	signer: Signer,
 ) => {
+	if (!signer.provider) {
+		throw new Error()
+	}
+
 	// Create the metadata
 	const payload = ItemMetadata.encode({ description })
 	const hash = await crypto.subtle.digest('SHA-256', payload)
 
 	// Get the marketplace contract
-	const contract = new Contract(marketplace, marketplaceAbi, signer)
+	const contract = getMarketplaceContract(marketplace, signer.provider)
 
 	// Get token decimals
 	const tokenAddress = await contract.token()
-	const token = new Contract(tokenAddress, erc20Abi, signer)
+	const token = getERC20Contract(tokenAddress, signer.provider)
 	const decimals = await token.decimals()
 
 	// Post the metadata on Waku
@@ -111,6 +113,11 @@ export const createItem = async (
 	// Get the item ID
 	const newItemTopic = contract.interface.getEventTopic('NewItem')
 	const newItemLog = logs.find((log: Log) => log.topics[0] === newItemTopic)
+
+	if (!newItemLog) {
+		throw new Error('no new item event in the transaction')
+	}
+
 	const { args } = contract.interface.parseLog(newItemLog)
 	return args.id.toBigInt()
 }
@@ -123,11 +130,19 @@ const decodeWakuMessage = async (message: WithPayload<MessageV0>): Promise<WakuI
 	}
 }
 
-export const useGetWakuItems = (marketplace: string) => {
-	const [items, setItems] = useState<WakuItem[]>([])
-	const [lastUpdate, setLastUpdate] = useState(Date.now())
+export const subscribeToWakuItems = (
+	waku: WakuLight,
+	marketplace: string,
+	callback: (item: WakuItem) => void,
+	onError?: (error: string) => void,
+	onDone?: () => void,
+) => {
+	let cancelled = false
+	const storeCallback = async (msg: Promise<MessageV0 | undefined>) => {
+		if (cancelled) {
+			return true
+		}
 
-	const callback = async (msg: Promise<MessageV0 | undefined>) => {
 		const message = await msg
 		if (!message?.payload) {
 			return
@@ -138,16 +153,17 @@ export const useGetWakuItems = (marketplace: string) => {
 			return
 		}
 
-		setItems((items) => [...items, decoded])
-		setLastUpdate(Date.now())
+		callback(decoded)
 	}
 
-	const topic = getItemTopic(marketplace)
-	const state = useWakuStoreQuery([new DecoderV0(topic)], callback, [topic])
+	waku.store
+		.queryCallbackOnPromise([new DecoderV0(getItemTopic(marketplace))], storeCallback)
+		.catch((error) => !cancelled && onError?.(error))
+		.finally(() => !cancelled && onDone?.())
 
-	useEffect(() => (state.loading ? setItems([]) : undefined), [state.loading])
-
-	return { ...state, lastUpdate, items }
+	return () => {
+		cancelled = true
+	}
 }
 
 const decodeNewItemEvent = async (event: Event, iface: Interface): Promise<ChainItem> => {
@@ -193,162 +209,190 @@ type Metadata = UpdateTime & {
 
 type MetadataIndex = Record<string, Metadata>
 
-export const useGetMarketplaceItems = (address: string) => {
-	const [loading, setLoading] = useState(true)
-	const [items, setItems] = useState<Record<string, ChainItem[]>>({})
-	const [lastUpdate, setLastUpdate] = useState(Date.now())
+export const subscribeToMarketplaceItems = (
+	address: string,
+	provider: Provider,
+	wsProvider: Provider,
+	callback: (items: Record<string, ChainItem[]>) => void,
+	onDone: () => void,
+) => {
+	// Contracts
+	const contract = getMarketplaceContract(address, provider)
+	const wsContract = getMarketplaceContract(address, wsProvider)
 
-	// Wagmi
-	const provider = useProvider()
-	const wsProvider = useWebSocketProvider()
-	const contract = useMemo(
-		() => new Contract(address, marketplaceAbi, provider),
-		[provider, address],
-	)
-	const wsContract = useMemo(
-		() => new Contract(address, marketplaceAbi, wsProvider),
-		[wsProvider, address],
-	)
+	// Data
+	const metadata: MetadataIndex = {}
+	const indexed: Record<string, ChainItem[]> = {}
 
-	useEffect(() => {
-		const metadata: MetadataIndex = {}
-		const indexed: Record<string, ChainItem[]> = {}
+	const updateMetadata = (
+		id: BigNumber,
+		metadata: string,
+		status: Status,
+		transactionHash: string,
+	) => {
+		for (const item of indexed[metadata]) {
+			if (item.id.eq(id)) {
+				item.status = status
+				item.transactions[status] = transactionHash
+			}
+		}
+	}
 
-		const updateMetadata = (
-			id: BigNumber,
-			metadata: string,
-			status: Status,
-			transactionHash: string,
-		) => {
-			for (const item of indexed[metadata]) {
-				if (item.id.eq(id)) {
-					item.status = status
-					item.transactions[status] = transactionHash
-				}
+	const updateProvider = (id: BigNumber, provider: string) => {
+		const data = metadata[id.toString()]
+		for (const item of indexed[data.metadata]) {
+			if (item.id.eq(id)) {
+				item.provider = provider
+			}
+		}
+	}
+
+	// Real time event listener
+	const statusListener = (id: BigNumber, status: Status, event: Event) => {
+		const data = metadata[id.toString()]
+
+		if (shouldUpdate(event, data)) {
+			updateMetadata(id, data.metadata, status, event.transactionHash)
+			callback(indexed)
+		}
+	}
+
+	const fundListener = (provider: string, id: BigNumber) => {
+		updateProvider(id, provider)
+		callback(indexed)
+	}
+
+	// eslint-disable-next-line @typescript-eslint/no-extra-semi
+	;(async () => {
+		const newItem = contract.interface.getEventTopic('NewItem')
+		const statusChange = contract.interface.getEventTopic('ItemStatusChange')
+		const fundItem = contract.interface.getEventTopic('FundItem')
+
+		// Listen to logs in real time
+		wsContract.on('ItemStatusChange', statusListener)
+		wsContract.on('FundItem', fundListener)
+
+		// Fetch historical
+		const events = await contract.queryFilter(
+			{
+				address: contract.address,
+				topics: [[newItem, statusChange, fundItem]],
+			},
+			0,
+		)
+
+		for (const event of events) {
+			const { blockNumber, transactionIndex, topics } = event
+
+			switch (topics[0]) {
+				case newItem:
+					const item = await decodeNewItemEvent(event, contract.interface)
+					metadata[item.id.toString()] = {
+						blockNumber,
+						transactionIndex,
+						metadata: item.metadata,
+					}
+					if (!indexed[item.metadata]) {
+						indexed[item.metadata] = []
+					}
+					indexed[item.metadata].push(item)
+					break
+
+				case statusChange:
+					// eslint-disable-next-line no-lone-blocks
+					{
+						const { id, status } = await decodeStatusChangeEvent(event, contract.interface)
+						const data = metadata[id.toString()]
+
+						if (shouldUpdate(event, data)) {
+							updateMetadata(id, data.metadata, status, event.transactionHash)
+						}
+					}
+					break
+
+				case fundItem:
+					// eslint-disable-next-line no-lone-blocks
+					{
+						const { id, provider } = await decodeFundItemEvent(event, contract.interface)
+						updateProvider(id, provider)
+					}
+
+					break
 			}
 		}
 
-		const updateProvider = (id: BigNumber, provider: string) => {
-			const data = metadata[id.toString()]
-			for (const item of indexed[data.metadata]) {
-				if (item.id.eq(id)) {
-					item.provider = provider
-				}
-			}
-		}
+		callback(indexed)
+		onDone()
+	})()
 
-		// Real time event listener
-		const statusListener = (id: BigNumber, status: Status, event: Event) => {
-			const data = metadata[id.toString()]
-
-			if (shouldUpdate(event, data)) {
-				updateMetadata(id, data.metadata, status, event.transactionHash)
-				setItems(indexed)
-				setLastUpdate(Date.now())
-			}
-		}
-
-		const fundListener = (provider: string, id: BigNumber) => {
-			updateProvider(id, provider)
-			setItems(indexed)
-			setLastUpdate(Date.now())
-		}
-
-		// eslint-disable-next-line @typescript-eslint/no-extra-semi
-		;(async () => {
-			const newItem = contract.interface.getEventTopic('NewItem')
-			const statusChange = contract.interface.getEventTopic('ItemStatusChange')
-			const fundItem = contract.interface.getEventTopic('FundItem')
-
-			// Listen to logs in real time
-			wsContract.on('ItemStatusChange', statusListener)
-			wsContract.on('FundItem', fundListener)
-
-			// Fetch historical
-			const events = await contract.queryFilter(
-				{
-					address: contract.address,
-					topics: [[newItem, statusChange, fundItem]],
-				},
-				0,
-			)
-
-			for (const event of events) {
-				const { blockNumber, transactionIndex, topics } = event
-
-				switch (topics[0]) {
-					case newItem:
-						const item = await decodeNewItemEvent(event, contract.interface)
-						metadata[item.id.toString()] = {
-							blockNumber,
-							transactionIndex,
-							metadata: item.metadata,
-						}
-						if (!indexed[item.metadata]) {
-							indexed[item.metadata] = []
-						}
-						indexed[item.metadata].push(item)
-						break
-
-					case statusChange:
-						// eslint-disable-next-line no-lone-blocks
-						{
-							const { id, status } = await decodeStatusChangeEvent(event, contract.interface)
-							const data = metadata[id.toString()]
-
-							if (shouldUpdate(event, data)) {
-								updateMetadata(id, data.metadata, status, event.transactionHash)
-							}
-						}
-						break
-
-					case fundItem:
-						// eslint-disable-next-line no-lone-blocks
-						{
-							const { id, provider } = await decodeFundItemEvent(event, contract.interface)
-
-							updateProvider(id, provider)
-						}
-
-						break
-				}
-			}
-
-			setItems(indexed)
-			setLoading(false)
-			setLastUpdate(Date.now())
-		})()
-
-		return () => {
-			wsContract.off('ItemStatusChange', statusListener)
-			wsContract.off('FundItem', fundListener)
-		}
-	}, [contract])
-
-	return { loading, items, lastUpdate }
+	return () => {
+		wsContract.off('ItemStatusChange', statusListener)
+		wsContract.off('FundItem', fundListener)
+	}
 }
 
-export const useMarketplaceItems = (marketplace: string) => {
-	const wakuItems = useGetWakuItems(marketplace)
-	const chainItems = useGetMarketplaceItems(marketplace)
-	const [items, setItems] = useState<Item[]>([])
-	const [lastUpdate, setLastUpdate] = useState(Date.now())
+export const subscribeToItems = (
+	waku: WakuLight,
+	provider: Provider,
+	wsProvider: Provider,
+	marketplace: string,
+	callback: (items: Item[]) => void,
+	onDone: () => void,
+) => {
+	// Loading
+	let wakuLoading = true
+	let chainLoading = true
 
-	useEffect(() => {
-		const itemsCopy = { ...chainItems.items }
-		const items = wakuItems.items.flatMap((item) => {
+	// Data
+	const wakuItems: WakuItem[] = []
+	let chainItems: Record<string, ChainItem[]>
+
+	// On update
+	const onUpdate = () => {
+		const itemsCopy = { ...chainItems }
+		const items = wakuItems.flatMap((item) => {
 			const events = itemsCopy[item.hash] || []
 			delete itemsCopy[item.hash]
 			return events.map((event) => ({ ...event, metadata: item.metadata }))
 		})
-		setItems(items)
-		setLastUpdate(Date.now())
-	}, [wakuItems.lastUpdate, chainItems.lastUpdate])
+		callback(items)
+	}
+	const onLoadingUpdate = () => {
+		if (!wakuLoading && !chainLoading) {
+			onDone()
+		}
+	}
 
-	return {
-		waiting: wakuItems.waiting,
-		loading: wakuItems.loading || chainItems.loading,
-		lastUpdate,
-		items,
+	// Subscribe to waku and chain items
+	const wakuUnsubscribe = subscribeToWakuItems(
+		waku,
+		marketplace,
+		(item: WakuItem) => {
+			wakuItems.push(item)
+			onUpdate()
+		},
+		undefined,
+		() => {
+			wakuLoading = false
+			onLoadingUpdate()
+		},
+	)
+	const chainUnsubscribe = subscribeToMarketplaceItems(
+		marketplace,
+		provider,
+		wsProvider,
+		(items) => {
+			chainItems = items
+			onUpdate()
+		},
+		() => {
+			chainLoading = false
+			onLoadingUpdate()
+		},
+	)
+
+	return () => {
+		wakuUnsubscribe()
+		chainUnsubscribe()
 	}
 }
