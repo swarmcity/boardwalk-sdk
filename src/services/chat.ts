e74db529c7baeeecde239c010d07eae2d5d149e9
Philippe Schommers <philippe@schommers.be>
2022-12-01 14:06:25 +0100

    feat: convert chat service

--- src/services/chat.ts
+++ src/services/chat.ts
@@ -1,297 +1,71 @@
-import { useEffect, useState } from 'react'
-import { useAsync } from 'react-use'
-import createStore from 'teaful'
 import { MessageV1, SymDecoder, SymEncoder } from 'js-waku/lib/waku_message/version_1'
-import { fromString } from 'uint8arrays/from-string'
 import { equals } from 'uint8arrays/equals'
-import { getPublicKey } from 'js-waku'
-import { bigintToBuf } from 'bigint-conversion'
-import { getSharedSecret } from 'noble-secp256k1'
-import { toString } from 'uint8arrays/to-string'
-
-// Lib
-import { readLocalStore, updateLocalStore } from '../lib/store'
+import pDefer from 'p-defer'
 
 // Services
-import { useWakuFilter, useWakuStoreQuery, WithPayload, wrapFilterCallback } from './waku'
+import { decodeStore, DecodeStoreCallback, subscribeToLatestTopicData } from '../lib/waku'
 
 // Types
 import type { WakuLight } from 'js-waku/lib/interfaces'
+import { WithPayload } from '../lib/types'
 
 // Protos
 import { ChatMessage as ChatMessageProto } from '../protos/chat-message'
-import { KeyExchange } from '../protos/key-exchange'
-
-// Store
-import { getStore as getMainStore } from '../store'
-
-// Config
-const PREFIX = 'chat'
-const KEY_TYPES = {
-	ecdh: {
-		size: 256,
-		prefix: new Uint8Array(0),
-	},
-	ecdsa: {
-		size: 256,
-		prefix: new Uint8Array(1),
-	},
-}
 
 // Types
-type TheirChatKeys = {
-	theirSigPubKey: Uint8Array
-	theirECDHPubKey: Uint8Array
-}
-
-type ChatKeys = Partial<TheirChatKeys> & {
-	temp?: Record<string, TheirChatKeys>
-}
-
-type ChatStore = {
-	keys: Record<string, ChatKeys>
-}
-
 type ChatMessage = {
 	date: Date
-	from: string
 	message: string
 }
 
-type FormattedChatKeys = {
-	symKey: Uint8Array
-	mySigPubKey: Uint8Array
-	theirSigPubKey: Uint8Array
-	mySigPrivKey: Uint8Array
+type PublicKeys = {
+	me: Uint8Array
+	them: Uint8Array
 }
 
-// Store
-const store = createStore<ChatStore>(
-	{
-		keys:
-			// eslint-disable-next-line @typescript-eslint/no-explicit-any
-			readLocalStore('keys', PREFIX, (key: any, value: any) => {
-				if (key === 'theirSigPubKey' || key === 'theirECDHPubKey') {
-					return fromString(value, 'base64url')
-				}
-				return value
-			}) ?? {},
-	},
-	({ store, prevStore }) => {
-		updateLocalStore(
-			store,
-			prevStore,
-			'keys',
-			PREFIX,
-			// eslint-disable-next-line @typescript-eslint/no-explicit-any
-			(key: any, value: any) => {
-				if (key === 'theirSigPubKey' || key === 'theirECDHPubKey') {
-					return toString(value, 'base64url')
-				}
-				return value
-			},
-		)
-	},
-)
-
-export const { useStore, getStore, withStore, setStore } = store
-
-export const deriveChatPrivateKey = async (
-	marketplace: string,
-	item: bigint,
-	type: keyof typeof KEY_TYPES,
-): Promise<Uint8Array> => {
-	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-	// @ts-expect-error
-	const [baseKey] = getMainStore.profile.chatBaseKey()
-	const base = await crypto.subtle.importKey('raw', baseKey, { name: 'HKDF' }, false, [
-		'deriveKey',
-		'deriveBits',
-	])
-
-	const address = marketplace.substring(2, Infinity).toLocaleLowerCase()
-	const salt = new Uint8Array([
-		...KEY_TYPES[type].prefix,
-		...fromString(address, 'hex'),
-		...new Uint8Array(bigintToBuf(item)),
-	])
-
-	return new Uint8Array(
-		await crypto.subtle.deriveBits(
-			{
-				name: 'HKDF',
-				hash: 'SHA-256',
-				salt: salt,
-				info: new Uint8Array(),
-			},
-			base,
-			KEY_TYPES[type].size,
-		),
-	)
+type PrivateKey = {
+	private: Uint8Array
 }
 
-export const getRecordKey = (marketplace: string, item: bigint) => {
-	return `${marketplace}:${item}`
-}
+type ChatMessageRes = DecodeStoreCallback<ChatMessage, MessageV1>
 
-const formatChatKeys = async (
-	marketplace: string,
-	item: bigint,
-	chatKeys: ChatKeys,
-): Promise<FormattedChatKeys | undefined> => {
-	if (!chatKeys?.theirSigPubKey || !chatKeys?.theirECDHPubKey) {
-		return
-	}
-
-	const ecdhPrivateKey = await deriveChatPrivateKey(marketplace, item, 'ecdh')
-	const ecdsaPrivateKey = await deriveChatPrivateKey(marketplace, item, 'ecdsa')
-	const symKey = await crypto.subtle.digest(
-		'SHA-256',
-		getSharedSecret(ecdhPrivateKey, chatKeys.theirECDHPubKey) as Uint8Array,
-	)
-
-	return {
-		symKey: new Uint8Array(symKey),
-		mySigPubKey: getPublicKey(ecdsaPrivateKey),
-		theirSigPubKey: chatKeys.theirSigPubKey,
-		mySigPrivKey: ecdsaPrivateKey,
-	}
-}
-
-const getChatKeys = async (marketplace: string, item: bigint) => {
-	const [chatKeys] = getStore.keys[getRecordKey(marketplace, item)]()
-	return formatChatKeys(marketplace, item, chatKeys)
-}
-
-const useChatKeys = (marketplace: string, item: bigint) => {
-	const [chatKeys] = useStore.keys[getRecordKey(marketplace, item)]()
-	return useAsync(async () => formatChatKeys(marketplace, item, chatKeys), [chatKeys])
-}
-
-export const setChatKey = async (
-	marketplace: string,
-	item: bigint,
-	type: Exclude<keyof ChatKeys, 'temp'>,
-	key: Uint8Array,
-) => {
-	setStore.keys[getRecordKey(marketplace, item)][type]?.(key)
-}
-
-export const setTheirChatKeys = async (
-	marketplace: string,
-	item: bigint,
-	{ sigPubKey, ecdhPubKey }: KeyExchange,
-) => {
-	setStore.keys[getRecordKey(marketplace, item)]((keys) => ({
-		...keys,
-		theirECDHPubKey: ecdhPubKey,
-		theirSigPubKey: sigPubKey,
-	}))
-}
-
-export const setTheirTempChatKeys = async (
-	marketplace: string,
-	item: bigint,
-	address: string,
-	keyExchange: KeyExchange,
-) => {
-	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-	// @ts-expect-error
-	setStore.keys[getRecordKey(marketplace, item)].temp((temp) => ({
-		...temp,
-		[address]: {
-			theirECDHPubKey: keyExchange.ecdhPubKey,
-			theirSigPubKey: keyExchange.sigPubKey,
-		},
-	}))
-}
-
-export const getKeyExchange = async (marketplace: string, item: bigint) => {
-	const ecdhPrivateKey = await deriveChatPrivateKey(marketplace, item, 'ecdh')
-	const ecdsaPrivateKey = await deriveChatPrivateKey(marketplace, item, 'ecdsa')
-
-	return {
-		sigPubKey: getPublicKey(ecdsaPrivateKey),
-		ecdhPubKey: getPublicKey(ecdhPrivateKey),
-	}
-}
-
-export const selectTempChatKey = (marketplace: string, item: bigint, address: string) => {
-	setStore.keys[getRecordKey(marketplace, item)]((keys) => {
-		if (!keys) {
-			return keys
-		}
-
-		if (keys.theirECDHPubKey && keys.theirSigPubKey) {
-			return keys
-		}
-
-		const theirKeys = keys.temp?.[address]
-		if (!theirKeys) {
-			return keys
-		}
-
-		delete keys.temp
-		return {
-			...keys,
-			...theirKeys,
-		}
-	})
+type ChatKeys = {
+	symmetric: Uint8Array
+	signing: PublicKeys & PrivateKey
+	encryption: PublicKeys & PrivateKey
 }
 
 export const getChatMessageTopic = (marketplace: string, item: bigint) => {
 	return `/swarmcity/1/chat-message-${marketplace}-${item}/proto`
 }
 
-const decodeWakuMessage = async (message: WithPayload<MessageV1>): Promise<ChatMessage> => {
+const decodeWakuMessage = async (
+	keys: PublicKeys,
+	message: WithPayload<MessageV1>,
+): Promise<ChatMessage | false> => {
+	if (!message.signaturePublicKey) {
+		return false
+	}
+
+	const decoded = ChatMessageProto.decode(message.payload)
+	const valid = (() => {
+		const users: Array<keyof PublicKeys> = ['me', 'them']
+		for (const user of users) {
+			if (equals(message.signaturePublicKey, keys[user])) {
+				return true
+			}
+		}
+		return false
+	})()
+
+	if (!valid) {
+		return false
+	}
+
 	return {
 		date: message.timestamp || new Date(),
-		from: '',
-		...ChatMessageProto.decode(message.payload),
+		...decoded,
 	}
-}
-
-export const useChatMessages = (marketplace: string, item: bigint) => {
-	const [items, setItems] = useState<ChatMessage[]>([])
-	const [lastUpdate, setLastUpdate] = useState(Date.now())
-
-	const { value: keys } = useChatKeys(marketplace, item)
-	const topic = getChatMessageTopic(marketplace, item)
-
-	const callback = async (msg: Promise<MessageV1 | undefined>) => {
-		if (!keys) {
-			return
-		}
-
-		const message = await msg
-		if (!message?.payload || !message.signaturePublicKey) {
-			return
-		}
-
-		const decoded = await decodeWakuMessage(message as WithPayload<MessageV1>)
-		if (!decoded) {
-			return
-		}
-
-		if (equals(message.signaturePublicKey, keys.mySigPubKey)) {
-			decoded.from = 'me'
-		} else if (equals(message.signaturePublicKey, keys.theirSigPubKey)) {
-			decoded.from = 'them'
-		} else {
-			return
-		}
-
-		setItems((items) => [...items, decoded])
-		setLastUpdate(Date.now())
-	}
-
-	const decoders = keys?.symKey ? [new SymDecoder(topic, keys.symKey)] : []
-	const state = useWakuStoreQuery(decoders, callback, [topic], undefined, !keys)
-	useWakuFilter(decoders, wrapFilterCallback(callback), [topic], {}, !!keys)
-
-	useEffect(() => (state.loading ? setItems([]) : undefined), [state.loading])
-
-	return { ...state, lastUpdate, items }
 }
 
 export const postChatMessage = async (
@@ -299,18 +73,47 @@ export const postChatMessage = async (
 	marketplace: string,
 	item: bigint,
 	message: { message: string },
+	keys: {
+		symmetric: Uint8Array
+		signing: PrivateKey
+	},
 ) => {
-	const keys = await getChatKeys(marketplace, item)
-	if (!keys) {
-		throw new Error('no symmetric key found')
-	}
-
 	// Create the protobuf message
 	const payload = ChatMessageProto.encode(message)
 
 	// Post the message on Waku
 	await waku.lightPush.push(
-		new SymEncoder(getChatMessageTopic(marketplace, item), keys.symKey, keys.mySigPrivKey),
+		new SymEncoder(getChatMessageTopic(marketplace, item), keys.symmetric, keys.signing.private),
 		{ payload },
 	)
+}
+
+export const subscribeToChatMessages = async (
+	waku: WakuLight,
+	marketplace: string,
+	item: bigint,
+	keys: ChatKeys,
+	callback: (response?: ChatMessageRes) => void,
+	watch = true,
+) => {
+	const topic = getChatMessageTopic(marketplace, item)
+	const decoders = [new SymDecoder(topic, keys.symmetric)]
+	subscribeToLatestTopicData(
+		waku,
+		decoders,
+		decodeStore(decodeWakuMessage.bind(null, keys.signing), callback),
+		{},
+		watch,
+	)
+}
+
+export const getChatMessages = async (
+	waku: WakuLight,
+	marketplace: string,
+	item: bigint,
+	keys: ChatKeys,
+): Promise<ChatMessageRes> => {
+	const defer = pDefer<ChatMessageRes>()
+	await subscribeToChatMessages(waku, marketplace, item, keys, defer.resolve, false)
+	return defer.promise
 }
