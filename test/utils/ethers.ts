91ea14d60af4228e118e55c42a6d7e348aed98ad
Philippe Schommers <philippe@schommers.be>
2022-12-02 21:38:00 +0100

    perf: use `StaticJsonRpcProvider`

--- test/utils/ethers.ts
+++ test/utils/ethers.ts
@@ -1,11 +1,11 @@
-import { JsonRpcProvider } from '@ethersproject/providers'
+import { StaticJsonRpcProvider } from '@ethersproject/providers'
 import { ContractTransaction, Wallet } from 'ethers'
 
 // Generated
 import { PRIVATE_KEYS } from '../test-addresses.json'
 
 export const getProvider = () => {
-	return new JsonRpcProvider('http://127.0.0.1:8545')
+	return new StaticJsonRpcProvider('http://127.0.0.1:8545')
 }
 
 export const getWallet = (index: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9) => {
