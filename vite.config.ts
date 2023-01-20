cd63a342e6aa65014c599c2d4afcc863df4b7d92
Philippe Schommers <philippe@schommers.be>
2023-01-20 15:48:12 +0100

    docs: add typedoc

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
+import { resolve } from 'path'
 import { configDefaults, defineConfig } from 'vitest/config'
 
+console.log('dirname', __dirname)
+
 export default defineConfig({
+	build: {
+		lib: {
+			entry: resolve(__dirname, 'src/index.ts'),
+			name: 'Boardwalk SDK',
+			fileName: 'boardwalk-sdk',
+		},
+	},
 	test: {
 		exclude: [...configDefaults.exclude, 'lib'],
 	},
